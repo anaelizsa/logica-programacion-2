a5ad2e765e03c2d67f0173e68432c7873f841565
Ana Elizondo-Salas <77133339+anaelizsa@users.noreply.github.com>
2025-03-13 15:32:03 -0600

    Update script.js

--- logica-programacion-2/script.js
+++ logica-programacion-2/script.js
@@ -16,17 +16,17 @@ const headline = document.createElement("h1")
 headline.innerText = "Conversión de Temperaturas";
 
 const textoTempC = document.createElement("h2")
-textoTempC.innerText = "Tempertatura en Celsius: " + tempC;
-console.log("Tempertatura en Celsius: " + tempC)
+textoTempC.innerText = "Tempertatura en Celsius: " + tempC + "°C";
+console.log("Tempertatura en Celsius: " + tempC + "°C")
 
 const textoTempK = document.createElement("h2")
-textoTempK.innerText = "Temperatura en Kelvin: " + tempK;
-console.log("Temperatura en Kelvin: " + tempK)
+textoTempK.innerText = "Temperatura en Kelvin: " + tempK + "K";
+console.log("Temperatura en Kelvin: " + tempK + "K")
 
 
 const textoTempF = document.createElement("h2")
-textoTempF.innerText = "Temperatura en Fahrenheit: " + tempF;
-console.log("Temperatura en Fahrenheit: " + tempF)
+textoTempF.innerText = "Temperatura en Fahrenheit: " + tempF + "°F";
+console.log("Temperatura en Fahrenheit: " + tempF + "°F")
 
 
 body.append(headline,textoTempC, textoTempK, textoTempF)
